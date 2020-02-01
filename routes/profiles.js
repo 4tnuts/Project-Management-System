6cdbeff99596212b35e4a84ea849ddc806edce3d
4tnuts <ramanavami32@gmail.com>
2020-02-02 04:26:04 +0700

    show profile data fixed

--- routes/profiles.js
+++ routes/profiles.js
@@ -4,15 +4,21 @@ const helpers = require('../helpers/util.js');
 
 module.exports= (pool) => {
     router.get('/', helpers.isLoggedIn, (req, res, next) => {
-        const userData = req.session.user;
-        res.render('profile/profile', {userData});
+        const selectQuery = 'SELECT * FROM users WHERE userid = $1';
+        const id = [req.session.user.userid];
+        pool.query(selectQuery, id, (err, result) => {
+            res.render('profile/profile', {userData : result.rows[0]});
+        })
     });
 
     router.post('/', helpers.isLoggedIn, (req, res, next) => {
             console.log(req.session.user.userid);
             console.log(req.session.user)
-            let updateQuery = 'UPDATE users SET email=$1, password=$2, position=$3, isfulltime=$4 WHERE userid=$5';
             const data = [req.body.email, req.body.password, req.body.position, req.body.isfulltime, req.session.user.userid];
+            if(req.body.password == ''){
+                data[1] = req.session.user.password;
+            }
+            let updateQuery = 'UPDATE users SET email=$1, password=$2, position=$3, isfulltime=$4 WHERE userid=$5';
             console.log(data);
             pool.query(updateQuery, data, (err, result) => {
                 if(err){
