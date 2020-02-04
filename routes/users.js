91cdbff47e10395253485aa63d0e8eab4b061c07
4tnuts <ramanavami32@gmail.com>
2020-02-05 01:41:36 +0700

    CRU need to check and delete need to fix

--- routes/users.js
+++ routes/users.js
@@ -7,48 +7,49 @@ router.use(bodyParser.json());
 
 module.exports = (pool) => {
   router.get('/',  (req, res, next) => {
-    res.render('users/dashboard');
+    let getDataUser = 'SELECT * FROM users'
+    pool.query(getDataUser, (err,result) => {
+      if (err) return console.error (err);
+      res.render('users/dashboard', result = {users: result.rows});
+    })
   });
 
   router.get('/add', (req, res, err) => {
     res.render('users/add');
   })
 
-  router.get('/edit', (req, res, err) => {
-    res.render('users/edit')
-  })
-
   router.post('/add', (req, res, next) => { 
     let insertQuery = 'INSERT INTO users(email, password, firstname, lastname, isfulltime, position) VALUES($1, $2, $3, $4, $5, $6)';
     let body = [req.body.email, req.body.password, req.body.firstname, req.body.lastname, req.body.isfulltime, req.body.position];
     pool.query(insertQuery, body, (err) => {
-      if(err) console.error(err);
-      res.redirect('users/add');
+      if (err) return console.error (err);
+      res.redirect('/users/add');
     });
   });
   
   router.get('/edit/:id', (req, res, next) => {
     const id = [req.params.id];
-    let getUserData = 'SELECT email, password, firstname, lastname, isfulltime, position WHERE userid=$1'
-    pool.query(getUserData, [id], (err, result) => {
-      if(err) res.render('error', err);
-      res.render('users')
+    let getUserData = 'SELECT userid, email, password, firstname, lastname, isfulltime, position FROM users WHERE userid=$1'
+    pool.query(getUserData, id, (err, result) => {
+      if (err) return console.error (err);
+      res.render('users/edit', result = {users : result.rows})
     })
   })
 
   router.post('/edit/:id', (req, res, next) => {
-    let queryUpdate = 'UPDATE SET email = $1, password = $2, firstname = $3, lastname = $4, isfulltime = $5, position = $6 WHERE userid = $7';
-    let body = [req.body.email, req.body.firstname, req.body.lastname, req.body.isfulltime, req.body.position, req.params.id];
+    let queryUpdate = 'UPDATE users SET email = $1, password = $2, firstname = $3, lastname = $4, isfulltime = $5, position = $6 WHERE userid = $7';
+    let body = [req.body.email,req.body.password, req.body.firstname, req.body.lastname, req.body.isfulltime, req.body.position, req.params.id];
     pool.query(queryUpdate, body, (err) => {
-      if (err) res.render ('error', err);
-      res.redirect(`edit/${req.params.id}`);
+      if (err) return console.error (err);
+      res.redirect(`/users/edit/${req.params.id}`);
     })
   })
 
   router.get('/delete/:id', (req, res, next) => {
     let deleteQuery = 'DELETE FROM users WHERE userid = $1';
     let id = [req.params.id];
-    pool.query(deleteQuery, id, (err, result) => {
+    pool.query(deleteQuery, id, (err) => {
+      if(err) return console.error(err);
       res.redirect('/users')
     })
   })
