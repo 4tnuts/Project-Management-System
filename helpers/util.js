5da6849334c3623a717110013ce0774ea29eeb9f
4tnuts <ramanavami32@gmail.com>
2020-03-11 20:06:36 +0700

    need to add admin privilige

--- helpers/util.js
+++ helpers/util.js
@@ -4,6 +4,12 @@ helpers = {
             return next();
         }
         res.redirect('/');
+    },
+    isAdmin : (req, res, next) => {
+        if(req.session.user.isAdmin == 'Admin'){
+         return next();   
+        }
+        res.redirect()
     }
 }
 
